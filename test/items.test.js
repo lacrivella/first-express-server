46ae454073dedba50f699544b00ef4846a7159e3
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-07-11 15:59:49 -0700

    post past test

--- test/items.test.js
+++ test/items.test.js
@@ -3,3 +3,17 @@ const app = require('../lib/app');
 
 //REMEMBER CRUD -- CREATE READ UPDATE DELETE
 
+describe('items routes', () => {
+  it('create a bird with POST', () => {
+    return request(app)
+      .post('/api/v1/birds')
+      .send({ type: 'bluejay', family: 'corvidae', color: 'blue'})
+      .then(res => {
+        expect(res.body).toEqual({
+          type: 'bluejay',
+          family: 'corvidae',
+          color: 'blue'
+        });
+      });
+  }); 
+});
